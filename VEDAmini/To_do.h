3ba249b726213fde5036ebfee93b36f5aa681305
epqpq <zedno15@gmail.com>
2024-08-01 09:56:39 +0900

    할 일 헤더파일 첫 작성.

---
+++ VEDAmini/To_do.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+
+#ifndef _TO_DO_H
+#define _TO_DO_H
+class ToDo
+{
+private:
+	std::vector<std::string> m_list;
+public:
+	ToDo();
+	~ToDo();
+	void insert(std::string input);
+	void modify(int idx, std::string input);
+	void del(int dix);
+	void allPrint() const;
+};
+#endif
